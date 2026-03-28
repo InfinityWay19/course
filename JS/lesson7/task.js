cfa5a04dc0c744fef1b9d654ccd77ecda29e1c78
InfinityWay19 <slesarev1930@gmail.com>
2026-03-28 17:01:50 +0600

    created a simple server with main page, list of rules and other stuff

---
+++ JS/lesson7/task.js
@@ -0,0 +1,31 @@
+const path = require("node:path");
+const http = require("node:http");
+const fs = require("node:fs/promises");
+
+const PORT = 3000;
+
+const sendFile = async (fileName, response, statusCode) => {
+    const fullPath = path.join(__dirname, "public", fileName);
+    const page = await fs.readFile(fullPath, "utf-8");
+
+    response.statusCode = statusCode;
+    response.end(page);
+}
+
+const server = http.createServer(async(request, response) => {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    if (request.url === "/") {
+        await sendFile("index.html", response, 200);
+    } else if (request.url === "/rules") {
+        await sendFile("rules.html", response, 200);
+    } else if (request.url === "/style") {
+        await sendFile("style.css", response, 200);
+    } else {
+        await sendFile("404.html", response, 404);
+        response.statusCode = 404;
+    }
+})
+
+server.listen(PORT);
+console.log(`server started on adress http://localhost:${PORT}`);
